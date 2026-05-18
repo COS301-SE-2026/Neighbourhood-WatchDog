6bfb30cc24547b1561e73c4a718f3db8ef804a36
JaredWilliams-1 <jaredluke3@gmail.com>
2026-05-18 10:45:14 +0200

    Added tests for the property schema

--- backend/app/schemas/property.py
+++ backend/app/schemas/property.py
@@ -1,16 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, StringConstraints
+from typing import Annotated
 from app.models.property import PropertyTypeEnum, Property
 from uuid import UUID
 from datetime import datetime
 
+NonEmptyString = Annotated[str, StringConstraints(min_length=1, strip_whitespace=True)]
+
 class CreatePropertyReq(BaseModel):
-    address: str
+    address: NonEmptyString
     property_type: PropertyTypeEnum
-    
+
 class PropertyRes(BaseModel):
     user_id: UUID
     neighbourhood_id: UUID | None
-    address: str
+    address: NonEmptyString
     property_type: PropertyTypeEnum
     created_at: datetime
 
