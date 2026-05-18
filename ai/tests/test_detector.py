70eb384cbddbc328cdcb828832e2374c2c281eb7
Joshua <mahabeerjoshua@gmail.com>
2026-05-18 22:56:02 +0200

    connected the camera stream to YOLOv8

--- ai/tests/test_detector.py
+++ ai/tests/test_detector.py
@@ -1,26 +1,34 @@
+import os
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;tcp"
+
 from ultralytics import YOLO
 
 model = YOLO('ai/pipeline/models/weights/yolov8n.pt')
 
-results = model.track(source="ai/tests/footage/clear-presence.mp4", 
+#original url (stream 1)
+# url = "rtsp://Intrepid:password1234@192.168.1.126:554/stream1"
+
+#url with stream 2, to lower resolution stream
+url = "rtsp://Intrepid:password1234@192.168.1.126:554/stream2"
+
+
+results = model.track(
+                # source="ai/tests/footage/clear-presence.mp4", 
+                source=url,
                 verbose=False, 
                 stream=True, 
                 conf=0.6, 
                 iou=0.3,
                 tracker="bytetrack.yaml",
-                persist=True)
+                persist=True,
+
+                #adding these to reduce overhead on cpu
+                imgsz=320, 
+                vid_stride=3) #processing every third frame to reduce load
 
 PERSON_CLASS = 0
 # human_count = 0
 unique_ids = set()
-
-# for result in results:
-#     for box in result.boxes:
-#         if int(box.cls[0]) == PERSON_CLASS:
-#             human_count += 1
-#             if box.id is not None:
-#                 unique_ids.add(int(box.id[0]))
-#             print(f"Human detected - confidence: {float(box.conf[0]):.2f}")
 
 
 for result in results:
