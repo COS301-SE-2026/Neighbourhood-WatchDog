b44eaa8809599a271eb3e894be772469e125cc9e
Joshua <mahabeerjoshua@gmail.com>
2026-05-19 07:56:00 +0200

    feat: implement detector and stream capture classes

--- ai/pipeline/processing/tracker.py
+++ ai/pipeline/processing/tracker.py
@@ -3,6 +3,8 @@ import os
 os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;tcp"
 
 from ultralytics import YOLO
+from deep_sort_realtime.deepsort_tracker import DeepSort
+
 
 PERSON_CLASS = 0
 
@@ -12,23 +14,48 @@ class Detector:
         self.conf = conf
         self.iou = iou
 
+        self.tracker = DeepSort(
+            max_age=70,
+            n_init=2,
+            max_iou_distance=0.7,
+            embedder="mobilenet",
+            embedder_gpu=False
+        )
+
     def process_frame(self, frame) -> list:
         results = self.model.track(
             frame,
             verbose=False, 
             conf=self.conf, 
             iou=self.iou,
-            tracker="bytetrack.yaml",
-            persist=True,
+            # tracker="bytetrack.yaml",
+            # persist=True,
+            imgsz=640,
         )
 
         detections = []
         for box in results[0].boxes:
             if int(box.cls[0]) == PERSON_CLASS and box.id is not None:
-                   detections.append({
-                        'track_id': int(box.id[0]),
-                        'confidence': float(box.conf[0]),
-                        'bbox': box.xyxy[0].tolist()
-                   })
+                x1, y1, x2, y2 = box.xyxy[0].tolist
+                confidence = float(box.conf[0])
+                detections.append((
+                    [x1, y1, x2-x1, y2-y1],
+                    confidence, 
+                    "person"
+                ))
 
-        return detections
+        tracks = self.tracker.update_tracks(detections, frame=frame)
+
+        confirmed = []
+        for track in tracks:
+            if not track.is_confirmed():
+                continue
+            confirmed.append({
+                'track_id': track.track_id,
+                'confidence': track.det_conf if track.det_conf is not None else 0.0,
+                'bbox': track.to_ltrb().tolist()
+            })
+
+        return confirmed
+
+        # return detections
