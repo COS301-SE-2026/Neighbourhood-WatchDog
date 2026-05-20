1c5ae5ad00d4c0051254c6c1e057ab9c494d8720
zamanbassa <zamanbassa@gmail.com>
2026-05-20 22:58:43 +0200

    Provided Layout and actual page for the forgot password page

---
+++ frontend/src/app/forgotpassword-page/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { ForgotPasswordCard } from "../../components/auth-components/forgetpassword-card";
+
+export default function Page() {
+  return (
+    <ForgotPasswordCard />
+  );
+}
