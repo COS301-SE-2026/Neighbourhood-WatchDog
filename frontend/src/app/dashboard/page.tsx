28e6556c5aae85df0e22ffcd7fe26df5fd8d7f04
Joshua <mahabeerjoshua@gmail.com>
2026-05-20 17:41:32 +0200

    updated dashboard page to be respodisplay camera stream

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -29,7 +29,9 @@ export default function Dashboard() {
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 <CameraCard name="Camera 1 - Backyard" status="offline" />
-                <CameraCard name="Camera 2 - Bedroom 1" status="online" />
+                {/* <CameraCard name="Camera 2 - Bedroom 1" status="online" /> */}
+
+                <CameraCard name="Camera 2 - Office Room 1" status="online" rtspUrl="rtsp://Intrepid:password1234@10.76.19.58:554/stream2"/>
                 <CameraCard name="Camera 5 - Living Room" status="online" />
                 <CameraCard name="Camera 3 - Bedroom 2" status="online" />
                 <CameraCard name="Camera 4 - Kitchen" status="online" />
