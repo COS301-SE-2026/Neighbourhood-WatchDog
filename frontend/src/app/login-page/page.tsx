05bd467c20924bf405e9720cac2b0c8c7f522c78
zamanbassa <zamanbassa@gmail.com>
2026-05-20 20:21:34 +0200

    Add LoginLayout and Page components for login functionality

---
+++ frontend/src/app/login-page/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { LoginCard } from "../../components/auth-components/login-card";
+
+export default function Page() {
+  return (
+    <div className="p-8 min-h-screen bg-gray-50">
+      <LoginCard />
+    </div>
+  );
+}
