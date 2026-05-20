f6cca4b7aa46deabe0ace8d740ca866eb85b0878
JaredWilliams-1 <jaredluke3@gmail.com>
2026-05-20 09:59:20 +0200

    Added to the sidebar

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,21 +1,57 @@
+"use client"
+
+import { LayoutDashboard, User } from "lucide-react"
+
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
 } from "@/components/ui/sidebar"
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu"
+
+const USERNAME = "John Doe"
 
 export function AppSidebar() {
   return (
-    <Sidebar>
-      <SidebarHeader />
+    <Sidebar className="bg-sidebar">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem className="text-white font-extrabold">
+            Neighbourhood WatchDog
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>  
         
       <SidebarContent>
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarMenuButton className="text-white hover:bg-blue-600 hover:text-white transition duration-fast">
+            <LayoutDashboard className="h-4 w-4" />
+            Dashboard
+          </SidebarMenuButton>
+        </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton className="text-white">
+              <User className="h-4 w-4"/>
+              { USERNAME }
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   )
 }
