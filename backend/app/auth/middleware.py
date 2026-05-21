650cbf4b9e793129bf11d6da37dc50a1f4604c0a
obedmbaya <obed.mbaya@yahoo.com>
2026-05-21 18:34:45 +0200

    add "/alerts" to public routes in AuthMiddleware for open access

--- backend/app/auth/middleware.py
+++ backend/app/auth/middleware.py
@@ -6,7 +6,7 @@ from app.core.config import config
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         #Checking the origin
-        public_routes = ["/health", "/docs", "/openapi.json", "/stream"]
+        public_routes = ["/health", "/docs", "/openapi.json", "/stream", "/alerts"]
 
         # Allow preflight requests without auth
         # TODO: remove this later
