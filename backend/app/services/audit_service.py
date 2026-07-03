e9ed98fe7ac6ca79da0f3d6482392aaae704c538
JaredWilliams-1 <jaredluke3@gmail.com>
2026-07-03 10:16:34 +0200

    Added validation to the create audit function

--- backend/app/services/audit_service.py
+++ backend/app/services/audit_service.py
@@ -1,30 +1,49 @@
 from fastapi import HTTPException
+from pydantic import Json, IPvAnyAddress
 from sqlalchemy import select
-from app.schemas.audit_log import AuditLogScheme
+from app.models.audit_log import AuditAction
 from app.models.user import User
 from app.models.audit_log import AuditLog
 from app.core.database import DbSession
 from sqlalchemy.exc import IntegrityError
-from uuid import uuid4
+from uuid import uuid4, UUID
+from datetime import datetime
 
 MOCK_IP_ADDRESS = "196.168.10.1"
 
-async def create_audit_log_item(audit_log_details: AuditLogScheme, db: DbSession) -> AuditLog:
+async def create_audit_log_item(user_id: UUID, action: AuditAction, target_entity_type: str, target_entity_id: UUID, ip_address: IPvAnyAddress, timestamp: datetime, extra_metadata, db: DbSession) -> AuditLog:
+    """Receives an AuditLogScheme object and adds the audit log to the database."""
     if not db:
         raise HTTPException(500, "No database session. Could not create audit log item.")
-    if not audit_log_details:
-        raise HTTPException(500, "No audit log details provided. Could not create audit log item.")
+    
+    if not user_id:
+        raise HTTPException(500, "No user id provided. Could not create audit log item.")
+    
+    if not action:
+        raise HTTPException(500, "No action provided. Could not create audit log item.")
+
+    if not target_entity_type:
+        raise HTTPException(500, "No target entity type provided. Could not create audit log item.")
+    
+    if not target_entity_id:
+        raise HTTPException(500, "No target entity id provided. Could not create audit log item.")
+    
+    if not ip_address:
+        raise HTTPException(500, "No IP Address provided. Could not create audit log item.")
+    
+    if not extra_metadata:
+        raise HTTPException(500, "No  provided. Could not create audit log item.")
 
     try:
         new_audit_log_item = AuditLog(
             id = uuid4,
-            user_id = audit_log_details.user_id,
-            action = audit_log_details.action,
-            target_entity_type = audit_log_details.target_entity_type,
-            target_entity_id = audit_log_details.target_entity_id,
-            ip_address = audit_log_details.ip_address | MOCK_IP_ADDRESS,
-            timestamp = audit_log_details,
-            extra_metadata = audit_log_details.extra_metadata
+            user_id = user_id,
+            action = action,
+            target_entity_type = target_entity_type,
+            target_entity_id = target_entity_id,
+            ip_address = ip_address | MOCK_IP_ADDRESS,
+            timestamp = timestamp,
+            extra_metadata = extra_metadata
         )
 
         db.add(new_audit_log_item)
--- tests/backend/unit/services/audit_service_test.py
+++ tests/backend/unit/services/audit_service_test.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi import HTTPException
 from unittest.mock import Mock
 from app.services.audit_service import create_audit_log_item
+from uuid import uuid4
 
 class TestCreateAuditLogItem:
     def setup_method(self):
@@ -13,6 +14,14 @@ class TestCreateAuditLogItem:
         self.mock_db.refresh = Mock()
         self.mock_db.rollback = Mock()
 
+        self.mock_log_item = Mock()
+        self.mock_log_item.user_id = uuid4()
+        self.mock_log_item.user_id = uuid4()
+
         self.mock_db.execute.return_value.scalar_one_or_none.return_value = None
 
-    
+    @pytest.mark.asyncio
+    async def test_happy_path(self):
+        audit_log_item = await create_audit_log_item(
+
+        )
