ee94f698aa0b3645c54fbb8e308f4e5494b73a36
JaredWilliams-1 <jaredluke3@gmail.com>
2026-07-04 12:57:36 +0200

    Made some changes to the validation of the audit creation function

--- tests/backend/unit/services/audit_service_test.py
+++ tests/backend/unit/services/audit_service_test.py
@@ -3,6 +3,7 @@ from fastapi import HTTPException
 from unittest.mock import Mock
 from app.services.audit_service import create_audit_log_item
 from app.models.audit_log import AuditAction
+from app.models.user import UserRole
 from uuid import uuid4
 from datetime import datetime
 
@@ -26,17 +27,37 @@ class TestCreateAuditLogItem:
     async def test_happy_path(self):
         audit_log_item = await create_audit_log_item(
             user_id=uuid4(),
-            action=AuditAction.LOGIN,
-            target_entity_type="TYPE",
+            action=AuditAction.CREATE,
+            target_entity_type="USER",
             target_entity_id=uuid4(),
             ip_address="196.168.21.2",
             timestamp=datetime.now(),
-            extra_metadata=(
-                {"old-value" : {"name": "John"}, "new-value": {"name" : "James"}}
-            )
+            old_values={
+                "id" : "f4b3e8c9-2d10-4f5c-b17a-59368dca86b2",
+                "email": "john@example.com",
+                "first_name": "John",
+                "last_name": "Doe",
+                "cognito_sub": "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+                "role": UserRole.RESIDENT,
+                "neighbourhood_id": "f4b3e8c9-2d10-4f5c-b17a-59368dca86b2",
+                "created_at": datetime.now()
+            },
+            new_value={
+                "id" : "f4b3e8c9-2d10-4f5c-b17a-59368dca86b2",
+                "email": "john@example.co.za", #changed the email address
+                "first_name": "John",
+                "last_name": "Doe",
+                "cognito_sub": "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+                "role": UserRole.RESIDENT,
+                "neighbourhood_id": "f4b3e8c9-2d10-4f5c-b17a-59368dca86b2",
+                "created_at": datetime.now()
+            }
         )
 
         assert self.mock_mb.add.call_count == 1
         assert self.mock_mb.refresh.call_count == 1
         assert self.mock_mb.commit.call_count == 1
         assert self.mock_db.rollback.call_count == 0
+
+
+        #TODO: include a case where the same new and old values are passed
