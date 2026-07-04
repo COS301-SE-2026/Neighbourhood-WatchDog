55de1cb560ad4549157692401b3eac0b5ac7619e
JaredWilliams-1 <jaredluke3@gmail.com>
2026-07-04 13:33:54 +0200

    Added audit service test for empty audit log item

--- tests/backend/unit/services/audit_service_test.py
+++ tests/backend/unit/services/audit_service_test.py
@@ -202,3 +202,39 @@ class TestCreateAuditLogItem:
         assert self.mock_db.add.call_count == 0
         assert self.mock_db.refresh.call_count == 0
         assert self.mock_db.commit.call_count == 0
+
+    @pytest.mark.asyncio
+    async def test_empty_target_entity_id(self):
+        with pytest.raises(HTTPException) as exception:
+            _ = await create_audit_log_item(
+                user_id=uuid4(),
+                action=AuditAction.UPDATE,
+                target_entity_type="USER",
+                target_entity_id=None,
+                old_values={
+                    "id" : "f4b3e8c9-2d10-4f5c-b17a-59368dca86b2",
+                    "email": "john@example.com",
+                    "first_name": "John",
+                    "last_name": "Doe",
+                    "cognito_sub": "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+                    "role": UserRole.RESIDENT,
+                    "neighbourhood_id": "f4b3e8c9-2d10-4f5c-b17a-59368dca86b2",
+                    "created_at": datetime.now()
+                },
+                new_values={
+                    "id" : "f4b3e8c9-2d10-4f5c-b17a-59368dca86b2",
+                    "email": "john@example.co.za", #changed the email address
+                    "first_name": "John",
+                    "last_name": "Doe",
+                    "cognito_sub": "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+                    "role": UserRole.RESIDENT,
+                    "neighbourhood_id": "f4b3e8c9-2d10-4f5c-b17a-59368dca86b2",
+                    "created_at": datetime.now()
+                },
+                db=self.mock_db
+            )
+
+        assert exception.value.status_code == 500
+        assert self.mock_db.add.call_count == 0
+        assert self.mock_db.refresh.call_count == 0
+        assert self.mock_db.commit.call_count == 0
