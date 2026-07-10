bd207a9f6efd7cabbd403a02703b11f75a71a25b
Zaman Bassa <zamanbassa@gmail.com>
2026-07-10 23:06:44 +0200

    fix: implement JWT authentication in AuthMiddleware and add verify_jwt function

---
+++ backend/app/auth/jwt.py
@@ -0,0 +1,37 @@
+from jose import jwt
+from jose.exceptions import JWTError
+
+import requests
+
+from app.core.config import config
+
+
+JWKS_URL = (f"https://cognito-idp.{config.aws_region}.amazonaws.com/{config.cognito_user_pool_id}/.well-known/jwks.json") #get public keys from AWS to verify
+
+ISSUER = (f"https://cognito-idp.{config.aws_region}.amazonaws.com/{config.cognito_user_pool_id}") #did this JWT come from our user pool
+
+
+JWKS = requests.get(JWKS_URL).json()["keys"] #public keys for user pool
+
+
+def verify_jwt(token: str) -> dict:
+    headers = jwt.get_unverified_header(token) #decode JWT headers
+
+    kid = headers.get("kid") #key id
+
+    key = next((k for k in JWKS if k["kid"] == kid), None) #find which primary key was used
+
+    if key is None:
+        raise JWTError("Unable to find matching public key.")
+
+    claims = jwt.decode(
+        token,
+        key,
+        algorithms=["RS256"],
+        issuer=ISSUER,
+        options={
+            "verify_aud": False
+        },
+    )
+
+    return claims
--- backend/app/auth/middleware.py
+++ backend/app/auth/middleware.py
@@ -1,6 +1,9 @@
+from jose import JWTError
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 from starlette.responses import JSONResponse
+
+from app.auth.jwt import verify_jwt
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
@@ -19,8 +22,25 @@ class AuthMiddleware(BaseHTTPMiddleware):
             any(request.url.path.startswith(p) for p in PUBLIC_PREFIXES)
         )
 
+        if is_public:
+            return await call_next(request)
+
+        auth_header = request.headers.get("Authorization")
+
         if not is_public:
-            if not request.headers.get("Authorization"):
+            if not auth_header:#does it have JWT
                 return JSONResponse({"detail": "No Authorization header"}, status_code=401)
+
+        if not auth_header.startswith("Bearer "):#extract JWT
+            return JSONResponse({"detail": "Invalid Authorization header"}, status_code=401)
+
+        token = auth_header.split(" ", 1)[1] #get jwt
+
+        try:
+            claims = verify_jwt(token)
+        except JWTError:
+            return JSONResponse({"detail": "Invalid or expired token"}, status_code=401)
+
+        request.state.claims = claims
 
         return await call_next(request)
