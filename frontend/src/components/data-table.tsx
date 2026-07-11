369c5d4f857b5c5dd37e6abe72f974cbb71c66be
JaredWilliams-1 <jaredluke3@gmail.com>
2026-07-11 19:07:02 +0200

    Making data table responsive

--- frontend/src/components/data-table.tsx
+++ frontend/src/components/data-table.tsx
@@ -15,15 +15,19 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { useIsMobile } from "@/hooks/use-mobile"
+import { Card } from "./ui/card"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
+  renderMobileCard?:  (rowData: TData) => React.ReactNode
 }
 
 export function DataTable<TData, TValue>({
   columns,
   data,
+  renderMobileCard
 }: DataTableProps<TData, TValue>) {
   const table = useReactTable({
     data,
@@ -31,7 +35,19 @@ export function DataTable<TData, TValue>({
     getCoreRowModel: getCoreRowModel(),
   })
 
-  return (
+  const isMobile = !!useIsMobile() && !!renderMobileCard
+
+  const cardsView = (
+    <div className="flex flex-col gap-3">
+      {data.map((rowData, index) => (
+        <Card key={index}>
+          {renderMobileCard && renderMobileCard(rowData)}
+        </Card>
+      ))}
+    </div>
+  )
+
+  const tableView = (
     <div className="overflow-hidden rounded-md border">
       <Table>
         <TableHeader>
@@ -76,5 +92,7 @@ export function DataTable<TData, TValue>({
         </TableBody>
       </Table>
     </div>
-  )
+    )
+
+  return isMobile? cardsView : tableView
 }
