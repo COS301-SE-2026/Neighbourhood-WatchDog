2b6e3169ebed54bafdb64efefb2f93d963a32f8e
Zaman Bassa <zamanbassa@gmail.com>
2026-07-11 16:29:29 +0200

    fix: clean up comments in jwt.py for clarity and consistency

--- backend/app/auth/jwt.py
+++ backend/app/auth/jwt.py
@@ -7,18 +7,14 @@ from app.core.config import config
 
 
 JWKS_URL = (f"https://cognito-idp.{config.aws_region}.amazonaws.com/{config.cognito_user_pool_id}/.well-known/jwks.json") #get public keys from AWS to verify
-
 ISSUER = (f"https://cognito-idp.{config.aws_region}.amazonaws.com/{config.cognito_user_pool_id}") #did this JWT come from our user pool
-
 
 JWKS = requests.get(JWKS_URL).json()["keys"] #public keys for user pool
 
 
 def verify_jwt(token: str) -> dict:
     headers = jwt.get_unverified_header(token) #decode JWT headers
-
-    kid = headers.get("kid") #key id
-
+    kid = headers.get("kid") #key id to find which public key used
     key = next((k for k in JWKS if k["kid"] == kid), None) #find which primary key was used
 
     if key is None:
