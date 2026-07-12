942dd0e0af9cd9e74baf8233bf4663986fa3bf1e
Joshua Mahabe <mahabeerjoshua@gmail.com>
2026-07-12 15:30:07 +0200

    testing: re configured camera id

--- ai/tests/test_detector.py
+++ ai/tests/test_detector.py
@@ -25,7 +25,7 @@ os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;tcp"
 # config
 MEDIAMTX_RTSP_URL = "rtsp://localhost:8554/tapo-camera"
 BACKEND_URL       = "http://localhost:8000"
-CAMERA_ID         = "2"
+CAMERA_ID         = "40000000-0000-0000-0000-000000000001"
 CONF_THRESHOLD    = 0.4
 IOU_THRESHOLD     = 0.3
 INFER_SIZE        = 320
