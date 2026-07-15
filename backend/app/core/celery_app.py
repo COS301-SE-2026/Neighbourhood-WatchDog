aa45bb9f2ca78e53b4c86ceda56d3a0ec0520b76
obedmbaya <obed.mbaya@yahoo.com>
2026-07-15 20:35:53 +0200

    added celery beat schedule to 5 min

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -3,6 +3,17 @@ import os
 from celery import Celery
 
 
-celery = Celery(__name__)
+celery = Celery(
+    __name__,
+    include=["app.tasks.risk_score_tasks"]
+)
+
 celery.conf.broker_url = os.environ.get("REDIS_URL")
 celery.conf.result_backend = os.environ.get("REDIS_URL")
+
+celery.conf.beat_schedule = {
+    "recalculate-risk-scores-every-5-minutes": {
+        "tasks": "app.tasks.risk_score_tasks.recalculate_all_risk_scores",
+        "schedule": 300.0,
+    }
+}
