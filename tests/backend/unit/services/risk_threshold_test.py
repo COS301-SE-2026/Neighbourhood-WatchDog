edc8700517e60483f86f28959e55ff973c827bb6
obedmbaya <obed.mbaya@yahoo.com>
2026-07-16 19:53:25 +0200

    testing for not authorised (get neighbourhood config)

--- tests/backend/unit/services/risk_threshold_test.py
+++ tests/backend/unit/services/risk_threshold_test.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from uuid import uuid4
+from uuid import UUID, uuid4
 
 import pytest
 from unittest.mock import Mock, patch
@@ -53,3 +53,25 @@ class TestRiskThresholdConfig:
         assert neighbourhood_risk_config.low_max == 20
         assert neighbourhood_risk_config.medium_max == 50
         assert neighbourhood_risk_config.updated_at == self.mock_risk_threshold_config.updated_at
+
+    @pytest.mark.asyncio
+    async def test_not_authorised(self):
+        """Not authorised for this neighbourhood"""
+
+        wrong_neighbourhood_id = UUID("717159e3-2ea3-4163-9773-e908fec43be6")
+
+        
+
+        with pytest.raises(HTTPException) as exception:
+            get_neighbourhood_risk_threshold_handler(
+                wrong_neighbourhood_id,
+                self.mock_db,
+                self.mock_claims
+            )
+
+        assert exception.value.status_code == 403
+
+        assert self.mock_db.execute.call_count == 0
+        assert self.mock_db.rollback.call_count == 0
+        assert self.mock_db.add.call_count == 0
+        assert self.mock_db.commit.call_count == 0
