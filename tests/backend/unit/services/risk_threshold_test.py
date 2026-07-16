5521060d65ccc3ba13d89f6679d82d85f209e37f
obedmbaya <obed.mbaya@yahoo.com>
2026-07-16 20:45:19 +0200

    Added testing for retreiving default global config

--- tests/backend/unit/services/risk_threshold_test.py
+++ tests/backend/unit/services/risk_threshold_test.py
@@ -20,16 +20,33 @@ class TestRiskThresholdConfig:
         self.mock_risk_threshold_config.medium_max = 50
         self.mock_risk_threshold_config.updated_at = datetime.now()
 
+        self.mock_default_threshold_config = Mock()
+        self.mock_default_threshold_config.id = uuid4()
+        self.mock_default_threshold_config.neighbourhood_id = None
+        self.mock_default_threshold_config.low_max = 30
+        self.mock_default_threshold_config.medium_max = 70
+        self.mock_default_threshold_config.updated_at = datetime.now()
+
 
         self.mock_db.execute.return_value.scalar_one_or_none.side_effect = [
             self.mock_risk_threshold_config,
         ]
+
+        self.mock_db.execute.return_value.scalar_one.side_effect = []
 
         self.mock_db.add = Mock()
         self.mock_db.flush = Mock()
         self.mock_db.refresh = Mock()
         self.mock_db.commit = Mock()
         self.mock_db.rollback = Mock()
+
+    def reset_side_effects(self, neighbourhood_threshold_config=None, global_default_config=None):
+        """Helper to reset side_effect between tests"""
+        self.mock_db.execute.return_value.scalar_one_or_none.side_effect = [
+            neighbourhood_threshold_config
+        ]
+        if global_default_config is not None:
+            self.mock_db.execute.return_value.scalar_one.side_effect = [global_default_config]
 
     @pytest.mark.asyncio
     async def test_happy_path(self):
@@ -75,3 +92,27 @@ class TestRiskThresholdConfig:
         assert self.mock_db.rollback.call_count == 0
         assert self.mock_db.add.call_count == 0
         assert self.mock_db.commit.call_count == 0
+
+    @pytest.mark.asyncio
+    async def test_no_neighbourhood_config(self):
+        self.reset_side_effects(
+            neighbourhood_threshold_config=None, 
+            global_default_config=self.mock_default_threshold_config
+        )
+
+        neighbourhood_risk_config = get_neighbourhood_risk_threshold_handler(
+            self.neighbourhood_id, 
+            self.mock_db, 
+            self.mock_claims
+        )
+
+        assert self.mock_db.execute.call_count == 2
+        assert self.mock_db.rollback.call_count == 0
+        assert self.mock_db.add.call_count == 0
+        assert self.mock_db.commit.call_count == 0
+
+        assert neighbourhood_risk_config is not None
+        assert neighbourhood_risk_config.neighbourhood_id is None
+        assert neighbourhood_risk_config.low_max == 30
+        assert neighbourhood_risk_config.medium_max == 70
+        assert neighbourhood_risk_config.updated_at == self.mock_default_threshold_config.updated_at
