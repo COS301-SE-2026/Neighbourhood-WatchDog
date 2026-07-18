cd91b9836c9cf59d945242b1fededa43bff74e99
JaredWilliams-1 <jaredluke3@gmail.com>
2026-07-18 14:21:09 +0200

    Merged two alembic versions

---
+++ backend/alembic/versions/217388f7f884_merge_heads.py
@@ -0,0 +1,29 @@
+"""merge heads
+
+Revision ID: 217388f7f884
+Revises: 24ebe8a57883, ed7933c51c73
+Create Date: 2026-07-18 14:19:56.580950
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+import geoalchemy2
+
+
+# revision identifiers, used by Alembic.
+revision: str = '217388f7f884'
+down_revision: Union[str, Sequence[str], None] = ('24ebe8a57883', 'ed7933c51c73')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
