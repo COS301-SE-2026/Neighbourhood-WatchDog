542986a4745f8fe3ef258de786cdd2fc99cb3ce9
Zaman Bassa <zamanbassa@gmail.com>
2026-07-18 10:38:43 +0200

    Updated imports fix

--- frontend/src/app/(public)/auth/forgotpassword/page.tsx
+++ frontend/src/app/(public)/auth/forgotpassword/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ForgotPasswordCard } from "../../../components/auth-components/forgetpassword-card";
+import { ForgotPasswordCard } from "@/components/auth-components/forgetpassword-card";
 
 export default function Page() {
   return (
