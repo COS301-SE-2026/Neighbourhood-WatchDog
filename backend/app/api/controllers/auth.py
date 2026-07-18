354a0e703e0379fe7dd15e8626bd0d23533cf544
JaredWilliams-1 <jaredluke3@gmail.com>
2026-07-18 08:57:06 +0200

    Changed type of error

--- backend/app/api/controllers/auth.py
+++ backend/app/api/controllers/auth.py
@@ -64,7 +64,7 @@ async def get_current_user_info(
     user =  db.execute(stmt).scalar_one_or_none()
 
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=401, detail="User not found")
     
     email = user.email
 
