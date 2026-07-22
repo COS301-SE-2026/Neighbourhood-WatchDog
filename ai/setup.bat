2233528069f0e470e71df23e292701248aaf92f5
zamanbassa <zamanbassa@gmail.com>
2026-07-22 10:08:42 +0200

    Add setup script for Neighbourhood Watchdog installation

---
+++ ai/setup.bat
@@ -0,0 +1,7 @@
+@echo off
+title Neighbourhood Watchdog Setup
+@REM Our user will click on this file and then START our script
+@REM We send the script to the Bootstrap.py
+python bootstrap.py 
+
+pause
