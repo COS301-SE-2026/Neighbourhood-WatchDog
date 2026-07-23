e7fed6598ff92316e592b2afb32c9551323691bc
obedmbaya <obed.mbaya@yahoo.com>
2026-07-23 16:38:31 +0200

    feature section of homepage done

--- frontend/src/app/(public)/home/page.tsx
+++ frontend/src/app/(public)/home/page.tsx
@@ -3,6 +3,7 @@
 import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
 import Introduction from "@/sections/Introduction";
+import Features from "@/sections/Features";
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             <Navbar/>
             <Hero/>
             <Introduction/>
+            <Features/>
         </>
     );
 
