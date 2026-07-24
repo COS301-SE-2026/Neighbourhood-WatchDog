3ff30b70bec2bd4d0786eabb810fa7d82012ff75
obedmbaya <obed.mbaya@yahoo.com>
2026-07-24 08:54:40 +0200

    integrations cameras section added

--- frontend/src/app/(public)/home/page.tsx
+++ frontend/src/app/(public)/home/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
 import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
+import Integrations from "@/sections/Integrations";
 
 export default function Home() {
     return (
@@ -12,6 +13,7 @@ export default function Home() {
             <Hero/>
             <Introduction/>
             <Features/>
+            <Integrations/>
         </>
     );
 
