63ab18fb92a732d828d0495bb22d6a2d73a4026c
JaredWilliams-1 <jaredluke3@gmail.com>
2026-07-24 12:24:17 +0200

    Corrected comment

--- ai/authenticator.py
+++ ai/authenticator.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import requests
 
-API_BASE_URL = "http://localhost:8000" #TODO: Change to EC2 instance from .env
+API_BASE_URL = "http://localhost:8000" #TODO: Change to server IP address
 
 
 class WatchDogPinPage(ttk.Frame):
