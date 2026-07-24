6b4d7cf192b5e33e2e9318ca3dd17bd470de2ab9
obedmbaya <obed.mbaya@yahoo.com>
2026-07-24 13:57:23 +0200

    footer addded

--- frontend/src/app/(public)/home/page.tsx
+++ frontend/src/app/(public)/home/page.tsx
@@ -6,6 +6,7 @@ import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
 import Integrations from "@/sections/Integrations";
 import Faqs from "@/sections/Faqs";
+import Footer from "@/sections/Footer";
 
 export default function Home() {
     return (
@@ -16,6 +17,7 @@ export default function Home() {
             <Features/>
             <Integrations/>
             <Faqs/>
+            <Footer/>
         </>
     );
 
