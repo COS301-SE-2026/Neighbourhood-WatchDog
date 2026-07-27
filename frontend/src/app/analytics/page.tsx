451137fa11fda0e900f53626131fa54f0cf68ac2
Ange <therealange.y@gmail.com>
2026-07-27 19:51:00 +0200

    Created analytics page

---
+++ frontend/src/app/analytics/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { AlertMetrics } from "@/components/shared/AlertMetrics";
+import { IncidentTrends } from "@/components/shared/IncidentTrends";
+import { AlertFrequencyGraph } from "@/components/shared/AlertFrequencyGraph";
+import { RiskScoreTrendGraph } from "@/components/shared/RiskScoreTrendGraph";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+
+export default function AnalyticsPage() {
+  return <></>;
+}
