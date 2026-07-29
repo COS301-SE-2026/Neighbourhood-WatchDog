c04359f19f920e4cf2f5019f519d2064aa2d6f16
Joshua Mahabe <mahabeerjoshua@gmail.com>
2026-07-29 18:01:31 +0200

    fix: alembic

---
+++ backend/alembic/versions/b21f39d5eeeb_merge_camera_and_analytics_migration_.py
@@ -0,0 +1,29 @@
+"""merge camera and analytics migration heads
+
+Revision ID: b21f39d5eeeb
+Revises: f4098eeef2e2, de4de5897f72
+Create Date: 2026-07-29 17:37:26.408180
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+import geoalchemy2
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b21f39d5eeeb'
+down_revision: Union[str, Sequence[str], None] = ('f4098eeef2e2', 'de4de5897f72')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
