b92358cda509bc9f46c30663befc6d181d88718f
Joshua Mahabe <mahabeerjoshua@gmail.com>
2026-07-29 18:23:36 +0200

    fix: parent analytics migration after camera schema update

--- backend/alembic/versions/1c4ab71bc39e_add_clip_columns_to_detection_event.py
+++ backend/alembic/versions/1c4ab71bc39e_add_clip_columns_to_detection_event.py
@@ -1,7 +1,7 @@
 """add clip columns to detection_event
 
 Revision ID: 1c4ab71bc39e
-Revises: c6da553e3309
+Revises: fdcaaba80ba7
 Create Date: 2026-07-11 06:19:16.709837
 """
 
@@ -13,10 +13,9 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "1c4ab71bc39e"
-down_revision: Union[str, Sequence[str], None] = "c6da553e3309"
+down_revision: Union[str, Sequence[str], None] = "fdcaaba80ba7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
-
 
 def upgrade() -> None:
     """Upgrade schema."""
