dafe965a4caabe242592d3b752ca7e605fc9120e
obedmbaya <obed.mbaya@yahoo.com>
2026-07-29 16:49:45 +0200

    changed to button for sonar

--- frontend/src/sections/Faqs.tsx
+++ frontend/src/sections/Faqs.tsx
@@ -48,10 +48,10 @@ export default function Faqs() {
                 <div className="mt-12 flex flex-col gap-6 max-w-xl mx-auto">
                     {faqs.map((faq, faqIndex) =>(
                         <div key={faq.question} className="bg-neutral-900 rounded-2xl border border-white/10 p-6">
-                            <div className="flex justify-between items-center" onClick={() => setselectedIndex(faqIndex)}>
+                            <button className="flex w-full justify-between items-center" onClick={() => setselectedIndex(faqIndex)}>
                                 <h3 className="font-medium">{faq.question}</h3>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={twMerge("feather feather-plus text-emerald-400 flex-shrink-0 transition duration-300", selectedIndex === faqIndex && "rotate-45")}><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
-                            </div>
+                            </button>
                             <AnimatePresence>
                                 {selectedIndex === faqIndex && (
                                     <motion.div initial={{height: 0, marginTop: 0,}} animate={{height: "auto", marginTop:24}} exit={{height: 0, marginTop: 0}} className={twMerge("overflow-hidden")}>
