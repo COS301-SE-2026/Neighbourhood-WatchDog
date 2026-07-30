a784e6b1357e8505fc6c9fecfefd2048ca4fed86
zamanbassa <zamanbassa@gmail.com>
2026-07-30 13:40:10 +0200

    Add Login, Signup, and Confirm cards to CardsSection component

--- frontend/src/app/designs/sections/cards.tsx
+++ frontend/src/app/designs/sections/cards.tsx
@@ -1,4 +1,8 @@
+"use client";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { LoginCard } from "@/components/auth-components/login-card";
+import { SignupCard } from "@/components/auth-components/signup-card";
+import { ConfirmCard } from "@/components/auth-components/confirm-card";
 
 export default function CardsSection() {
   return (
@@ -14,6 +18,53 @@ export default function CardsSection() {
           Base shadcn styling
         </CardContent>
       </Card>
+
+      {/* Login card */}
+      <LoginCard
+        email="john@example.com"
+        setEmail={() => {}}
+        password="password123"
+        setPassword={() => {}}
+        onSubmit={() => {}}
+        onKeyDown={() => {}}
+        isLoading={false}
+        error={null}
+      />
+
+      <SignupCard
+        firstName= "Name"
+        setFirstName={() => {}}
+        lastName={"Surname"}
+        setLastName={() => {}}
+        email={"email@email.com"}
+        setEmail={() => {}}
+        password={"Password"}
+        setPassword={() => {}}
+        confirmPassword={"Password"}
+        setConfirmPassword={() => {}}
+        address={"Dummy Address"}
+        setAddress={() => {}}
+        onSubmit={() => {}}
+        onKeyDown={() => {}}
+        isLoading={false}
+        error={null}
+      />
+
+      <ConfirmCard
+        email={"email@email.com"}
+        setEmail={() => {}}
+        code={"676767"}
+        setCode={() => {}}
+        onSubmit={() => {}}
+        onResendCode={() => {}}
+        onKeyDown={() => {}}
+        isLoading={false}
+        isResending={false}
+        error={null}
+        success={false}
+        resendSuccess={false}
+      />
+
 
     </section>
   );
