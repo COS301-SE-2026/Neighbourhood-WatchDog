c3adafe442f6cb7c355e5db652baab0e3aea1c38
JaredWilliams-1 <jaredluke3@gmail.com>
2026-07-31 01:58:44 +0200

    Fixed CORS mistake

--- backend/main.py
+++ backend/main.py
@@ -54,7 +54,7 @@ app.add_middleware(SlowAPIMiddleware) #Rate limiting
 
 app.add_middleware( #CORS (allow requests from frontend)
     CORSMiddleware,
-    allow_origins=[config.frontend_url.rstrip("/"), "http://localhost:3000", "https://neighbourhood-watch-dog-intrepidcapstone-4790-teamintrepid.vercel.app", "https://api.neighbourhoodwatchdog.co.za/auth/signup"],
+    allow_origins=[config.frontend_url.rstrip("/"), "http://localhost:3000", "https://neighbourhood-watch-dog-intrepidcapstone-4790-teamintrepid.vercel.app", "https://api.neighbourhoodwatchdog.co.za"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
