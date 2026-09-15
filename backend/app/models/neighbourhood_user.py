8fa4ca78f0f21aa2ad505b8f4bba377a639c179e
JaredWilliams-1 <jaredluke3@gmail.com>
2026-09-15 08:23:24 +0200

    Added security officer table with availability attribute

--- backend/app/models/neighbourhood_user.py
+++ backend/app/models/neighbourhood_user.py
@@ -10,11 +10,6 @@ class NeighbourhoodRole(str, Enum):
     NEIGHBOURHOOD_ADMIN = "NEIGHBOURHOOD_ADMIN"
     SECURITY_OFFICER = "SECURITY_OFFICER"
 
-class AvailabilityStatus(str, Enum):
-    AVAILABLE = "AVAILABLE"     # officer is ready to receive alerts
-    BUSY = "BUSY"               # officer is busy with an incident but an alert can be added to the queue
-    UNAVAILABLE = "UNAVAILABLE" # officer cannot receive alerts right now  
-
 class NeighbourhoodUser(Base):
     __tablename__ = "neighbourhood_user"
     
@@ -22,11 +17,11 @@ class NeighbourhoodUser(Base):
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     neighbourhood_id = Column(UUID(as_uuid=True), ForeignKey("neighbourhood.id", ondelete="CASCADE"), nullable=False)
     role = Column(SAEnum(NeighbourhoodRole, name="neighbourhood_role"), nullable=False)
-    availability_status = Column(SAEnum(AvailabilityStatus, name="availability_status"), nullable=True, default=AvailabilityStatus.UNAVAILABLE) # this is for security officers
     
     # Relationships
     user = relationship("User", back_populates="neighbourhood_memberships")
     neighbourhood = relationship("Neighbourhood", back_populates="user_memberships")
+    security_officer = relationship("SecurityOfficer", back_populates="neighbourhood_user", uselist=False)
     
     __table_args__ = (
         Index("uq_neighbourhood_user_unique", "user_id", "neighbourhood_id", unique=True),
