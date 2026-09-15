468ba31351fa82ebd77e0a03911c5b551e59d431
JaredWilliams-1 <jaredluke3@gmail.com>
2026-09-15 08:23:51 +0200

    Fixed linting errors in migration file

--- backend/alembic/versions/ad46ced5f06b_added_security_officer_table_with_.py
+++ backend/alembic/versions/ad46ced5f06b_added_security_officer_table_with_.py
@@ -8,8 +8,6 @@ Create Date: 2026-09-15 08:22:42.048246
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
-import geoalchemy2
 
 
 # revision identifiers, used by Alembic.
