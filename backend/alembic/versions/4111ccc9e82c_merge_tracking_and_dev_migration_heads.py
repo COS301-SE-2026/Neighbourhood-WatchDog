9962bbc9560f2899db35ed90f04daeb770db0fd6
Joshua Mahabe <mahabeerjoshua@gmail.com>
2026-09-18 15:37:30 +0200

    ci: python lint and test

--- backend/alembic/versions/4111ccc9e82c_merge_tracking_and_dev_migration_heads.py
+++ backend/alembic/versions/4111ccc9e82c_merge_tracking_and_dev_migration_heads.py
@@ -7,9 +7,7 @@ Create Date: 2026-09-18 15:21:26.162266
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-import geoalchemy2
+
 
 
 # revision identifiers, used by Alembic.
