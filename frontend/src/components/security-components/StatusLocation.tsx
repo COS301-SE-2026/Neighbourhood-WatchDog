50059d4734984deae92816aa77a25447fb6b412e
JaredWilliams-1 <jaredluke3@gmail.com>
2026-09-21 19:22:23 +0200

    Fixed linting errors

--- frontend/src/components/security-components/StatusLocation.tsx
+++ frontend/src/components/security-components/StatusLocation.tsx
@@ -100,6 +100,7 @@ export default function StatusToggle({ neighbourhoodId }: StatusToggleInterface)
   })
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     if (fullyGranted) setPermissionBlocked(false)
   }, [fullyGranted])
 
