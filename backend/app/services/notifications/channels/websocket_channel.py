f80a4b04c7bdf1b352d77a0e86b88178d0d4aec9
JaredWilliams-1 <jaredluke3@gmail.com>
2026-09-24 20:25:14 +0200

    Added to WebSocket skeleton

--- backend/app/services/notifications/channels/websocket_channel.py
+++ backend/app/services/notifications/channels/websocket_channel.py
@@ -1,1 +1,5 @@
 from app.services.notifications.channel import NotificationChannel
+
+class WebSocketChannel(NotificationChannel):
+    async def send():
+        pass #TODO: implement. This one aint adding to the queue this one sends straight via WebSocket
