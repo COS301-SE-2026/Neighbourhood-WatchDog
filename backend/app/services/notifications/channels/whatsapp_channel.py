e7a3de30491bcedd9ef5681fa229e1f89b45e5c7
JaredWilliams-1 <jaredluke3@gmail.com>
2026-09-24 20:26:30 +0200

    Added to WhatsApp channel class skeleton

--- backend/app/services/notifications/channels/whatsapp_channel.py
+++ backend/app/services/notifications/channels/whatsapp_channel.py
@@ -1,1 +1,5 @@
 from app.services.notifications.channel import NotificationChannel
+
+class WhatsAppChannel(NotificationChannel):
+    async def send():
+        pass #TODO: implement. This would add it to the queue
