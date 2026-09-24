c21a22944660aa94b16708606eee3f752fcab58b
JaredWilliams-1 <jaredluke3@gmail.com>
2026-09-24 20:23:56 +0200

    Added to skeleton of the email channel class

--- backend/app/services/notifications/channels/email_channel.py
+++ backend/app/services/notifications/channels/email_channel.py
@@ -1,1 +1,5 @@
 from app.services.notifications.channel import NotificationChannel
+
+class EmailChannel(NotificationChannel):
+    async def send():
+        pass #TODO: implement. This would add it to the queue
