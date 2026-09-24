f6926c6dcabd472a11e33ca5d81e20271b8f2b6f
JaredWilliams-1 <jaredluke3@gmail.com>
2026-09-24 20:20:39 +0200

    Added skeleton of policy builder

---
+++ backend/app/services/notifications/policy_builder.py
@@ -0,0 +1,25 @@
+from collections.abc import Callable
+
+from app.services.notifications.channel import NotificationChannel
+from app.services.notifications.policy import NotificationPolicy
+
+class NotificationPolicyBuilder:
+    def __init__(
+        self,
+        channels: list[NotificationChannel],
+        recipient_resolver: Callable,
+        severity_fn: Callable,
+    ):
+        pass
+
+    async def with_channel(NotificationChannel):
+        pass #TODO we finna add to this
+
+    async def with_recipient_resolver(fn):
+        pass #TODO: implement
+
+    async def with_severity(fn):
+        pass #TODO: implement
+
+    async def build() -> NotificationPolicy:
+        pass # TODO: implement
