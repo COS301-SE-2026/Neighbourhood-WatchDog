8d11e2f181a5f44d085fadc2885460cad1d1ec2b
JaredWilliams-1 <jaredluke3@gmail.com>
2026-09-24 16:23:59 +0200

    Added concrete class files and skeleton

--- backend/app/services/notifications/channel.py
+++ backend/app/services/notifications/channel.py
@@ -1,6 +1,17 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from uuid import UUID
 
 from app.core.database import DbSession
+from app.models.user import User
 
 class NotificationChannel(ABC):
     """The abstract parent class for the notifcation channels"""
+    @abstractmethod
+    async def send(
+        self,
+        db: DbSession,
+        notification_id: UUID,
+        recipients: list[User],
+        context: dict,
+    ) -> None:
+        
