7e1f5e4ec400011e37fea6768228a5889a212841
JaredWilliams-1 <jaredluke3@gmail.com>
2026-09-24 20:24:12 +0200

    Added to skeleton of the push channel class

--- backend/app/services/notifications/channels/push_channel.py
+++ backend/app/services/notifications/channels/push_channel.py
@@ -1,1 +1,5 @@
 from app.services.notifications.channel import NotificationChannel
+
+class PushChannel(NotificationChannel):
+    async def send():
+        pass #TODO: implement. This would add it to the queue
